0d168e7005508650982fcd6abd1e9f2096e39f32
Tushar Nachan <tushar.nachan@cloudsynapps.com>
2025-06-11 17:51:33 +0530

    Commented out Unleash initialization for Heroku deployment

--- config/initializers/unleash.rb
+++ config/initializers/unleash.rb
@@ -1,8 +1,8 @@
-Unleash.configure do |config|
-  config.app_name = 'webapp'
-  config.url      = ENV.fetch('UNLEASH_URL', nil)
-  config.logger   = Rails.logger
-  config.custom_http_headers = { Authorization: ENV.fetch('UNLEASH_TOKEN_RAILS', nil) }
-end
-
-Rails.configuration.unleash = Unleash::Client.new
+# Unleash.configure do |config|
+#   config.app_name = 'webapp'
+#   config.url      = ENV.fetch('UNLEASH_URL', nil)
+#   config.logger   = Rails.logger
+#   config.custom_http_headers = { Authorization: ENV.fetch('UNLEASH_TOKEN_RAILS', nil) }
+# end
+#
+# Rails.configuration.unleash = Unleash::Client.new
